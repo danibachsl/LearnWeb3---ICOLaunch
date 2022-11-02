7ef8bc1def99777dbe53573f2178eedce209a6fd
Dani Bachs <danibachsl@protonmail.com>
2022-11-02 12:34:27 +0100

    commiting ICO project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xfC3683d617721490eBb003c5158e9dFAF515F69B";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
